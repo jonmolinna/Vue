416a5c3975771b4108094e6125f4d15bca89af9e
jonmolinna <jmolina2624@gmail.com>
2021-07-06 17:31:37 -0500

    Vue Route

---
+++ 4.-vue-route/src/views/Fotos.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <h1>Galeria de Fotos</h1>
+        <router-link 
+            :to="{ name: 'Fotos', params: {id : item}}"  
+            v-for="(item, index) of fotosArreglo" :key="index"
+        >
+            <!-- name: Fotos => sale de como pusiste en ruote index--->
+            <button>Foto {{ item }}</button>
+        </router-link>
+
+        <Foto />
+
+        <button @click="home">Home</button>
+        <button @click="anterior">Anterior</button>
+        <button @click="siguiente">Siguiente</button>
+
+    </div>
+</template>
+
+<script>
+import Foto from "@/components/Fotografia.vue";
+
+export default {
+    components: {
+        Foto
+    },
+    data() {
+        return {
+            fotosArreglo: [1, 2, 3, 4]
+        }
+    },
+    methods: {
+        home(){
+            this.$router.push('/')
+        },
+        anterior(){
+            this.$router.go(-1)
+        },
+        siguiente(){
+            this.$router.go(1)
+        },
+    }
+}
+</script>
---
+++ 4.-vue-route/src/views/Servicios.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+       <h1>Esta es una pagina de servicios</h1> 
+    </div>
+</template>
