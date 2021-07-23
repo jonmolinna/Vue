a700ac9ad6084c10ca2f2e37b4938e0e12b2abfe
jonmolinna <jmolina2624@gmail.com>
2021-07-23 11:50:45 -0500

    Vue Ejercicios Basico

---
+++ 3.-vue-ejercicios/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col mt-5">
+        <CrudApi />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import CrudApi from './components/CrudApi.vue';
+
+export default {
+  name: 'App',
+  components: {
+    CrudApi  
+  }
+}
+</script>
+
+<style>
