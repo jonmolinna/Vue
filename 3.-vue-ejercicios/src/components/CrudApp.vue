f6432054e0c4463f0888332d16ef5ad5ce2bad7f
jonmolinna <jmolina2624@gmail.com>
2021-08-02 11:16:18 -0500

    CRUD APP

---
+++ 3.-vue-ejercicios/src/components/CrudApp.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h3>CRUD APP</h3>
+        <div class="row">
+            <div class="col-12 col-md-6 mb-3">
+                <CrudAppForm />
+            </div>
+            <div class="col-12 col-md-6">
+                <CrudAppTable />
+            </div>
+        </div>        
+    </div>
+</template>
+
+<script>
+import CrudAppForm from './CrudAppForm.vue';
+import CrudAppTable from './CrudAppTable.vue'
+
+export default {
+    components: {
+        CrudAppForm,
+        CrudAppTable
+    }
+};
+</script>
