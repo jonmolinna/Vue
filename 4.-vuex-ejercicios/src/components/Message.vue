3c3acd083434562164317dc26a4d363361b16eac
jonmolinna <jmolina2624@gmail.com>
2021-08-09 09:50:37 -0500

    Message

---
+++ 4.-vuex-ejercicios/src/components/Message.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="message">
+        <p>{{msg}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["msg", "bgColor"]
+};
+</script>
+
+<style scoped>
+    .message {
+        padding: 1rem;
+        margin-bottom: 1rem;
+        text-align: center;
+        color: #fff;
+        font-weight: bold;
+        background-color: #dc3545;
+    }
+</style>
