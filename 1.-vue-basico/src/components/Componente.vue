3385b9f486440622a7f6907906d2a92d96b20a76
jonmolinna <jmolina2624@gmail.com>
2021-08-09 11:14:33 -0500

    Componentes y Paso de Props

---
+++ 1.-vue-basico/src/components/Componente.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h2>Componentes y Paso de Props</h2>
+        <ul>
+            <ComponenteHijo 
+                v-for="item in groceryList"
+                v-bind:key="item.id"
+                v-bind:todo="item"
+            />
+        </ul>
+        
+    </div>
+</template>
+
+<script>
+import ComponenteHijo from './ComponenteHijo.vue';
+
+export default {
+    data() {
+        return {
+            groceryList: [
+                { id: 1, text: "JavaScript Vanilla"},
+                { id: 2, text: "React Js"},
+                { id: 3, text: "Vue Js"},
+                { id: 4, text: "Angular Js"},
+            ]
+        };
+    },
+    components: {
+        ComponenteHijo
+    }
+};
+</script>
---
+++ 1.-vue-basico/src/components/ComponenteHijo.vue
@@ -0,0 +1,16 @@
+<template>
+    <li>
+        {{ todo.text }}
+    </li>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            
+        };
+    },
+    props: ['todo']
+}
+</script>
