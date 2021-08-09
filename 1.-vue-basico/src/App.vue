1949cee92a7e17624d7994c5955cca70db8ae247
jonmolinna <jmolina2624@gmail.com>
2021-08-09 11:15:14 -0500

    Vue 2 Basico

---
+++ 1.-vue-basico/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <DeclarativeRendering />
+    <hr>
+    <Condicionales />
+    <hr>
+    <Methods />
+    <hr>
+    <Componente />
+  </div>
+</template>
+
+<script>
+import DeclarativeRendering from'./components/DeclarativeRendering.vue';
+import Condicionales from './components/Condicionales.vue';
+import Methods from './components/Methods.vue';
+import Componente from './components/Componente.vue';
+
+export default {
+  name: 'App',
+  components: {
+    DeclarativeRendering,
+    Condicionales,
+    Methods,
+    Componente
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
