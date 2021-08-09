123fe6115fb7a2b834f9a94fc654f5f2995069e9
jonmolinna <jmolina2624@gmail.com>
2021-08-09 09:55:53 -0500

    store

---
+++ 4.-vuex-ejercicios/src/store/index.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import crudApi from '../modules/crudApi';
+import crudApp from '../modules/crudApp';
+import artist from '../modules/artist';
+import selectList from '../modules/selectList';
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+    crudApi,
+    crudApp,
+    artist,
+    selectList
+  }
+})
