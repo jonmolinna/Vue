b4148551231862debb521fa46075dc92e25a4d89
jonmolinna <jmolina2624@gmail.com>
2021-08-09 09:52:41 -0500

    Artist Search

---
+++ 4.-vuex-ejercicios/src/components/ArtistSearch.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+        <h3>Buscador de Artista</h3>
+        <div class="row">
+            <div class="col-12 col-md-6 mb-3">
+                <ArtistForm />  
+            </div>
+            <div class="col-12 col-md-6">
+                <Loader v-if="loading === true" />
+                <ArtistDetails v-if="!loading && artist" />
+            </div>
+        </div>
+  </div>
+</template>
+
+<script>
+import ArtistForm from './ArtistForm.vue';
+import ArtistDetails from './ArtistDetails.vue';
+import Loader from './Loader.vue';
+import { mapActions, mapGetters } from 'vuex';
+
+export default {
+    components: {
+        ArtistForm,
+        ArtistDetails,
+        Loader,
+    },
+    methods: {
+        ...mapActions(['getArtist']),
+    },
+    computed: {
+        ...mapGetters(['loading', 'artist']),
+    },
+};
+</script>
