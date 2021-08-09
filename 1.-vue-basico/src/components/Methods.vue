0a822fbc5fd26b0e802b3eda8cd38f23873df8f7
jonmolinna <jmolina2624@gmail.com>
2021-08-09 11:13:44 -0500

    Metodos (v-on y v-model)

---
+++ 1.-vue-basico/src/components/Methods.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h2>Metodos (v-on y v-model)</h2>
+
+        <p>{{ message }}</p>
+
+        <button v-on:click="reverseMessage">Reverse Message </button>
+        <br>
+        <br>
+        <input type="text" v-model="message2">
+        <p>{{ message2 }}</p>
+
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: 'Hola Vue',
+            message2: '',
+        };
+    },
+    methods: {
+        reverseMessage: function(){
+            this.message = this.message.split('').reverse().join('');
+        },
+    }
+};
+</script>
