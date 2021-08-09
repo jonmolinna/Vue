5947974146468bbb64964ae8ee43bd5d187c9cae
jonmolinna <jmolina2624@gmail.com>
2021-08-09 09:52:18 -0500

    Artist Search

---
+++ 4.-vuex-ejercicios/src/components/ArtistForm.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="card">
+        <div class="card-body">
+            <form @submit="searchArtist">
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Ingrese Artista..." v-model="name">
+                </div>
+                <div class="form-group">
+                    <button class="btn btn-success form-control">Buscar</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+    data(){
+        return {
+            name: "",
+        }
+    },
+    methods: {
+        ...mapActions(['getArtist']),
+
+        searchArtist(e){
+            e.preventDefault();
+            if(!this.name) return window.alert("Ingrese Nombre del Artista");
+            this.getArtist(this.name);
+            this.name = "";
+        },
+    }
+};
+</script>
