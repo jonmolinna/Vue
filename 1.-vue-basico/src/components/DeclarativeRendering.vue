1f9415c38b5c9d8e762ebfefdd9add2b793e196e
jonmolinna <jmolina2624@gmail.com>
2021-08-09 11:13:21 -0500

    Primeros Pasos

---
+++ 1.-vue-basico/src/components/DeclarativeRendering.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h2>Representacion Declarativa</h2>
+
+        <h4>{{ message }}</h4>
+
+        <span v-bind:title="message2">
+            Hover your mouse over me for a few seconds to see my dynamically bound title
+        </span>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            message: 'Hola Mundo, Primeros Pasos',
+            message2: 'You loaded this page on ' + new Date().toLocaleString(),
+        }
+    }
+};
+</script>
