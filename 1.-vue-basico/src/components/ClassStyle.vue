605a5c486365860a3ed30556618c1ece1eaa9b6d
jonmolinna <jmolina2624@gmail.com>
2021-08-11 11:36:35 -0500

    Estilos

---
+++ 1.-vue-basico/src/components/ClassStyle.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <h2>Estilos</h2>
+
+        <p v-bind:class="{active: isActive}">Lorem, ipsum dolor.</p>
+        <p class="static" v-bind:class="{active: isActive, 'text-danger': hasError}">Texto con estilos</p>
+
+        <p v-bind:class="[activeClass, errorClass]">Lorem ipsum dolor sit.</p>
+        <p v-bind:class="[isActive? activeClass: '', errorClass]">Lorem, ipsum dolor.</p>
+        <p v-bind:class="[{ active: isActive}, errorClass]">Lorem ipsum dolor sit.</p>
+
+        <h2>Estilos en Linea</h2>
+        <p v-bind:style="styleObject">Lorem ipsum dolor sit.</p>
+        <p v-bind:style="[styleObject, borderObject]">Lorem ipsum dolor sit.</p>
+
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            isActive: true,
+            hasError: true,
+
+            activeClass: 'active',
+            errorClass: 'text-danger',
+
+            styleObject: {
+                color: 'blue',
+                fontSize: '20px'
+            },
+            borderObject: {
+                border: '3px solid black',
+                backgroundColor: 'brown',
+            }
+        };
+    },
+};
+</script>
+
+<style scope>
+    .active {
+        background-color: cadetblue;
+    }
+    .static {
+        border: 3px solid black;
+    }
+    .text-danger {
+        color: brown;
+    }
+</style>
