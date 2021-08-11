ae15423b5e77bc93fe1680a33b7c42717dc36b95
jonmolinna <jmolina2624@gmail.com>
2021-08-11 11:37:17 -0500

    Eventos

---
+++ 1.-vue-basico/src/components/Eventos.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h2>Eventos</h2>
+        <p>{{ contador }}</p>
+        <button v-on:click="sumar">+</button>
+
+        <form v-on:submit.prevent="onSubmit">
+            <button>Enviar</button>
+        </form>
+
+        <input type="text" placeholder="Ingrese tu datos" v-on:keyup.enter="onSubmit">
+
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            contador: 0,
+        };
+    },
+    methods: {
+        sumar(){
+            this.contador++
+        },
+        onSubmit(){
+            alert('Enviando Mensaje');
+        }
+    }
+};
+</script>
