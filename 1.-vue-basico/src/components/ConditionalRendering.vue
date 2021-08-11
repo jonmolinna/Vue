88bf3032a6eb916023b6adc002f3119872c2f733
jonmolinna <jmolina2624@gmail.com>
2021-08-11 11:37:01 -0500

    v-if - v-else - v-show

---
+++ 1.-vue-basico/src/components/ConditionalRendering.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <h2>v-if - v-else - v-show</h2>
+        <ol>
+            <li>El uso de v-if y v-show son el mismo</li>
+            <li>v-if elimina el elemento y v-show oculta el elemento</li>
+        </ol>
+        <div v-if="ok">
+            <h4>Titulo 1</h4>
+            <p>Lorem ipsum dolor sit.</p>
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+        </div>
+
+        <div v-if="numero > 10">
+            <p>El numero es mayor a 10, y me ves</p>
+        </div>
+        <div v-else>
+            <p>El numero es menor a 10, y me ves</p>
+        </div>
+
+        <div v-if="letra === 'A'">
+            <p>Es la letra A</p>
+        </div>
+        <div v-else-if="letra === 'B'">
+            <p>Es la letra B</p>
+        </div>
+        <div v-else>
+            <p>No es la A ni la B</p>
+        </div>
+
+        <div v-show="ok">
+            <h4>Titulo 1</h4>
+            <p>Lorem ipsum dolor sit.</p>
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            ok: true,
+            numero: 17,
+            letra: 'C'
+        };
+    },
+};
+</script>
