05c8323fa5d71a09c500986610a1f0afe49b323b
jonmolinna <jmolina2624@gmail.com>
2024-05-14 09:44:52 -0500

    Custom Directivas

--- 2-life-cicle/src/components/DirectivasCustom.vue
+++ 2-life-cicle/src/components/DirectivasCustom.vue
@@ -1,7 +1,10 @@
 <template>
     <div>
         <p v-font-size>Lorem ipsum dolor sit.</p>
-        <p v-custom-size="40">Lorem ipsum dolor sit.</p>
+        <p v-custom-size="40">Lorem ipsum dolor sit 2.</p>
+        <p v-custom-sizes:xl>Lorem ipsum dolor sit 3.</p>
+        <p v-custom-font.lg.red>Lorem, ipsum dolor. 4</p>
+        <p v-custom-font.green.xxl>Lorem, ipsum dolor. 4</p>
     </div>
 </template>
 
--- 2-life-cicle/src/main.ts
+++ 2-life-cicle/src/main.ts
@@ -6,20 +6,23 @@ import App from "./App.vue";
 const app = createApp(App);
 
 // Directivas Globales
+// Directivas personalizadas simple
 app.directive("font-size", {
   beforeMount: (element, binding) => {
     element.style.fontSize = "50px";
   },
 });
 
+// Directivas personalizadas con valores
 app.directive("custom-size", {
   beforeMount: (element, binding) => {
     element.style.fontSize = binding.value + "px";
   },
 });
 
-// v-bind:[class, key, style]
-app.directive("custom-size", {
+// Directivas personalizadas con argumentos
+// v-bind:[class, key, style] - Argumentos
+app.directive("custom-sizes", {
   beforeMount: (element, binding) => {
     let size = 18;
     switch (binding.arg) {
@@ -40,6 +43,31 @@ app.directive("custom-size", {
     }
 
     element.style.fontSize = size + "px";
+  },
+});
+
+// Directivas con modificadores
+app.directive("custom-font", {
+  beforeMount: (element, binding) => {
+    let size = 18;
+
+    if (binding.modifiers.sm) {
+      size = 10;
+    } else if (binding.modifiers.lg) {
+      size = 25;
+    } else if (binding.modifiers.xxl) {
+      size = 72;
+    }
+
+    element.style.fontSize = size + "px";
+
+    if (binding.modifiers.red) {
+      element.style.color = "#ff0000";
+    } else if (binding.modifiers.green) {
+      element.style.color = "#00ff00";
+    } else if (binding.modifiers.blue) {
+      element.style.color = "#0000ff";
+    }
   },
 });
 
