aae4fa5b43bbaa28a40216086deb2e453976c3f6
jonmolinna <jmolina2624@gmail.com>
2024-06-12 10:07:42 -0500

    Auth Social

--- 6-authentication/src/views/SocialView.vue
+++ 6-authentication/src/views/SocialView.vue
@@ -1,20 +1,21 @@
 <template>
     <h3>Social Login</h3>
     <div class="content">
-        <button @click="handleLogin">Login with Google</button>
+        <button @click="handleLoginWithGoogle">Login with Google</button>
         <button>Login with Facebook</button>
         <button>Login with Twitter</button>
-        <button>Login with Github</button>
+        <button @click="handleLoginWithGitHub">Login with Github</button>
     </div>
 </template>
 
 <script lang="ts" setup>
-    import { GoogleAuthProvider, signInWithPopup, getAuth } from 'firebase/auth';
+    import { GoogleAuthProvider, signInWithPopup, getAuth, GithubAuthProvider } from 'firebase/auth';
 
     const googleProvider = new GoogleAuthProvider();
     const auth = getAuth();
+    const githubProvider = new GithubAuthProvider();
 
-    const handleLogin = () => {
+    const handleLoginWithGoogle = () => {
         signInWithPopup(auth, googleProvider)
             .then(result => {
                 // ToKen
@@ -40,6 +41,28 @@
                 // El tipo de AuthCredential que se utilizo
                 const credential = GoogleAuthProvider.credentialFromError(error);
                 console.log('Error 3', credential); 
+            })
+    };
+
+    const handleLoginWithFacebook = () => {
+        // Facebook Developer
+    }
+
+    const handleLoginWithGitHub = () => {
+        signInWithPopup(auth, githubProvider)
+            .then(result => {
+                const credential = GithubAuthProvider.credentialFromResult(result);
+                const token = credential?.accessToken;
+
+                const user = result.user;
+
+                console.log({token, user});
+            })
+            .catch(error => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+
+                console.log('ERRor', {errorCode, errorMessage});
             })
     }
 
