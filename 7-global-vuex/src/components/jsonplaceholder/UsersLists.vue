7e5b1b1e838ba7e2a8e964c00514dac9b7c79025
jonmolinna <jmolina2624@gmail.com>
2024-07-04 10:43:56 -0500

    CRUD API

--- 7-global-vuex/src/components/jsonplaceholder/UsersLists.vue
+++ 7-global-vuex/src/components/jsonplaceholder/UsersLists.vue
@@ -1,11 +1,18 @@
 <template>
     <div class="container">
+        <div class="flex">
+            <form class="form" @submit.prevent="handleSubmit">
+                <input type="text" placeholder="Name" v-model="initialForm.name">
+                <input type="email" placeholder="Email" v-model="initialForm.email">
+                <button type="submit">Enviar</button>
+            </form>
+        </div>
         <div class="grid">
             <div v-for="user of users" :key="user.id" class="card">
                 <h2 class="card_title">{{ user.name }}</h2>
                 <span class="card_span">{{ user.email }}</span>
                 <div class="card_btns">
-                    <button class="card_btn">Update</button>
+                    <button class="card_btn" @click="findUserById(user.id)">Update</button>
                     <button class="card_btn" @click="deleteUser(user.id)">Delete</button>
                 </div>
             </div>
@@ -14,18 +21,55 @@
 </template>
 
 <script lang="ts" setup>
+    import user from '@/data/user.data';
     import { userInterface } from '@/interface/user.interface';
-    import { Ref, computed, onMounted } from 'vue';
+    import { Ref, computed, onMounted, ref, watch } from 'vue';
     import { useStore } from 'vuex';
 
     const store = useStore();
 
+    interface formInterface {
+        name: string;
+        email: string;
+        id: number;
+    }
+
+    const initialForm: Ref<formInterface> = ref({ name: '', email: '', id: 0 });
+
     const gellUsers = () => store.dispatch('getAllUsers');
     const deleteUser = (id: number) => store.dispatch('deleteUserById', id);
+    const addUser = (user: userInterface) => store.dispatch('addUser', user);
+    const findUserById = (id: number) => store.commit('findUserById', id);
+    const updatedUser = (user: userInterface) => store.dispatch('updatedUser', user);
+
+    const userEdit: Ref<userInterface> = computed(() => store.state.userApiModule.user);
+    
+    const handleSubmit = () => {
+        if (initialForm.value.id !== 0) {
+            let userData = userEdit.value;
+            let userUpdated: userInterface = {...userData, id: userData.id, email: initialForm.value.email, name: initialForm.value.name }
+            updatedUser(userUpdated);
+        } else {
+            const newUser: userInterface = {...user, id: new Date().getTime(), name: initialForm.value.name, email:initialForm.value.email };
+            addUser(newUser);
+        }
+
+        initialForm.value.email = "";
+        initialForm.value.name = "";
+        initialForm.value.id = 0;
+    }
 
     onMounted(() => {
         gellUsers();
     });
+
+    watch(userEdit, (user) => {
+        if (user) {
+            initialForm.value.email = user.email;
+            initialForm.value.name = user.name;
+            initialForm.value.id = user.id
+        }
+    })
 
     const users: Ref<userInterface[]> = computed(() => store.state.userApiModule.users);
 </script>
@@ -87,5 +131,32 @@
     .card_btn:last-child {
         background-color: #be123c;
     }
+
+    .flex {
+        display: flex;
+        justify-content: center;
+        
+    }
+
+    .form {
+        display: flex;
+        flex-direction: column;
+        margin-bottom: 20px;
+        width: 300px;
+    }
+
+    .form > input {
+        margin-bottom: 10px;
+        padding: .4rem;
+    }
+
+    .form > button {
+        padding: .4rem;
+        background-color: #059669;
+        color: white;
+        border: none;
+        cursor: pointer;
+    }
+
 
 </style>
--- 7-global-vuex/src/store/users.api.module.ts
+++ 7-global-vuex/src/store/users.api.module.ts
@@ -6,11 +6,13 @@ const url = "https://jsonplaceholder.typicode.com/users";
 
 interface stateInterface {
   users: userInterface[];
+  user: userInterface | null;
 }
 
 // State
 const state: stateInterface = {
   users: [],
+  user: null,
 };
 
 // Getters
@@ -19,6 +21,32 @@ const state: stateInterface = {
 const mutations: MutationTree<stateInterface> = {
   getAllUsers(state, payload: userInterface[]) {
     state.users = payload;
+  },
+  deleteUserById(state, payload: number) {
+    state.users = state.users.filter((user) => user.id !== payload);
+  },
+  addUser(state, payload: userInterface) {
+    state.users = [payload, ...state.users];
+  },
+  findUserById(state, payload: number) {
+    const user: userInterface | undefined = state.users.find(
+      (user) => user.id === payload
+    );
+
+    if (user) {
+      state.user = user;
+    }
+  },
+  updatedUser(state, payload: userInterface) {
+    const user = state.users.find((item) => item.id === payload.id);
+
+    if (user) {
+      state.users = state.users.map((item) =>
+        item.id === user.id
+          ? { ...item, email: payload.email, name: payload.name }
+          : item
+      );
+    }
   },
 };
 
@@ -38,9 +66,33 @@ const actions: ActionTree<stateInterface, any> = {
 
   async deleteUserById({ commit }, user: number) {
     await axios
-      .delete(`https://jsonplaceholder.typicode.com/users/${user}`)
+      .delete(`${url}/${user}`)
       .then((response) => {
-        const user = response.data;
+        const res = response.data;
+        commit("deleteUserById", user);
+      })
+      .catch((error) => {
+        console.log("Error", error);
+      });
+  },
+
+  async addUser({ commit }, user: userInterface) {
+    await axios
+      .post(url, user)
+      .then((response) => {
+        commit("addUser", response.data);
+      })
+      .catch((error) => {
+        console.log("Error", error);
+      });
+  },
+
+  async updatedUser({ commit }, user: userInterface) {
+    const { id, ...rest } = user;
+    await axios
+      .put(`${url}/${id}`, user)
+      .then((response) => {
+        commit("updatedUser", response.data);
       })
       .catch((error) => {
         console.log("Error", error);
